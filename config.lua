7eee05e671facc7d58ea793b5ca1c3ea80cd5866
z0041uxv <danieleberle@siemens.com>
2024-02-26 18:18:56 +0100

    Tried to get py lsp for .venv working...

--- config.lua
+++ config.lua
@@ -10,3 +10,10 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+
+require'py_lsp'.setup {
+  -- This is optional, but allows to create virtual envs from nvim
+  host_python = "C:\\rgc-tools\\Environment\\python\\39-64",
+  default_venv_name = ".venv" -- For local venv
+}
---
+++ plugins/pylsp.lua
@@ -0,0 +1,5 @@
+return {
+    'HallerPatrick/py_lsp.nvim',
+    -- Support for versioning
+    -- tag = "v0.0.1" 
+}
