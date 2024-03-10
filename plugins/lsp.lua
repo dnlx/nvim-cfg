72bd2c7d307620193cfbb511bed74f4e3f2e7045
Daniel <d.eberle.alex@gmail.com>
2024-03-10 20:18:45 +0100

    Changed hotkeys for auto completion.

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -48,6 +48,8 @@ return {
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
         'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
         'folke/neodev.nvim'
     },
     config = function()
@@ -55,6 +57,15 @@ return {
         require('neodev').setup({
           -- add any options here, or leave empty to use the default settings
         })
+
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities()
+        )
         require('fidget').setup({})
         require('mason').setup()
         require('mason-lspconfig').setup({
@@ -67,6 +78,35 @@ return {
                 end,
             }
         })
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        cmp.setup({
+            snippet = {
+              -- REQUIRED - you must specify a snippet engine
+              expand = function(args)
+                require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+              end,
+            },
+            window = {
+              -- completion = cmp.config.window.bordered(),
+              -- documentation = cmp.config.window.bordered(),
+            },
+
+            mapping = cmp.mapping.preset.insert({
+                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'vsnip' }, -- For vsnip users.
+              -- { name = 'luasnip' }, -- For luasnip users.
+              -- { name = 'ultisnips' }, -- For ultisnips users.
+              -- { name = 'snippy' }, -- For snippy users.
+            }, {
+              { name = 'buffer' },
+            })
+          })
     end
 }
 
