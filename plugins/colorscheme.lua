4c4d15681652d977b98db8c392a6c74aa661d9e2
Eberle <danieleberle@siemens.com>
2025-11-25 16:41:21 +0100

    Changed to live grep and removed background

--- plugins/colorscheme.lua
+++ plugins/colorscheme.lua
@@ -20,5 +20,8 @@ return {
     })
 
     vim.cmd('colorscheme github_dark_default')
+
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   end,
 }
--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -20,9 +20,10 @@ return {
             })
         end, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fg', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        -- vim.keymap.set('n', '<leader>fg', function()
+        --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        -- end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
